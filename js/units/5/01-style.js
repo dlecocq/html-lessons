f6dba4746a27aa08e07ae809d662b9beb9b0bc48
Dan Lecocq <dan.lecocq@kaust.edu.sa>
2012-01-29 13:11:02 -0800

    Updated the introduction of the style attribute.

--- js/units/5/01-style.js
+++ js/units/5/01-style.js
@@ -8,20 +8,42 @@ addLesson(Object({
 	title       : 'Stylin\'',
 	id          : 'style',
 	description : "\
-Beyond these text transformations, you can apply even more with    \
-something called <strong>style</strong>. Styles can be applied to  \
-a tag using its style attribute, or through <strong>CSS</strong>   \
-(cascading style sheets). Styles are a list of key:value pairs,    \
-separated by ';' (semicolons). You can change the color of text    \
-with the 'color' style like so: <pre>&lt;h1 style='color:blue'&gt;\
-This is a Blue Header&lt/h1&gt;</pre>\
-Use the 'color' style to change this header's color to red.",
-	input       : '<h1 style="">Red Head(er)</h1>',
-	solution    : '<h1 style="color:red">Red Head(er)</h1>',
+<p>Rather than using tags like <span class='cm-tag'>&lt;big&gt;</span> \
+and <span class='cm-tag'>&lt;del&gt;</span> to change the way text is \
+displayed, a more popular approach is to use styles. HTML elements support \
+a <span class='cm-atom'>style</span> attribute that describes how it \
+should be displayed.</p>\
+<p>The contents of the style attribute has its own syntax. In it's simplist \
+form, it has the name of a style, and a value separated by a colon. \
+For example, the text in the paragraph <span class='cm-tag'>&lt;p</span> \
+<span class='cm-atom'>style</span>=<span class='cm-comment'>\"color: red\"</span>\
+<span class='cm-tag'>&gt;</span>Hello!<span class='cm-tag'>&lt;/p&gt;</span> would \
+be displayed red.</p>\
+<p>We'll learn more styling options shortly, but for now, use the \
+<span class='cm-atom'>style</span> attribute to make the text of the headers \
+red</p>",
+	
+	input       : '<h1 style="">This Header Should Be Red</h1>\n\
+<h1>As Should This One</h1>\n\
+<h1>Oh, and This One</h1>',
+	
+	solution    : '<h1 style="color:red">This Header Should Be Red</h1>\n\
+<h1 style="color:red">As Should This One</h1>\n\
+<h1 style="color:red">Oh, and This One</h1>',
+
 	pass        : [],
+	
 	fail        : [],
+	
 	valid       : function(input) {
 		var item = $(input);
-		return item.is('h1') && item.attr('style').match(/color\s*:\s*red/);
+		var result = true;
+		item.filter('h1').each(function(index, el) {
+			if (!$(el).attr('style').match(/color\s*:\s*red/)) {
+				result = false;
+				return false;
+			}
+		});
+		return result;
 	}
 }));
